20bf87bf0536821aa06876b80b5dcecd567977d7
MohammedGhaly <medooghaly7@gmail.com>
2024-02-02 14:37:11 +0200

    sorting games

--- src/Components/SortSelector.tsx
+++ src/Components/SortSelector.tsx
@@ -1,19 +1,38 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-function SortSelector() {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const sortOrders = [
+  { value: "", label: "Relevance" },
+  { value: "-added", label: "Date added" },
+  { value: "name", label: "Name" },
+  { value: "-released", label: "Release date" },
+  { value: "-metacritic", label: "Popularity" },
+  { value: "-rating", label: "Average Rating" },
+];
+
+function SortSelector({ onSelectSortOrder, sortOrder }: Props) {
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order By:
+        Order By: {currentSortOrder?.label || ""}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevence</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Average Rating</MenuItem>
-        <MenuItem>Date Added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Release Date</MenuItem>
+        {sortOrders.map((item) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(item.value)}
+            key={item.value}
+          >
+            {item.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/Services/image-url.ts
+++ src/Services/image-url.ts
@@ -1,4 +1,5 @@
 function getCroppedUrl(url: string, width: number = 600, height: number = 400) {
+  if (!url) return "";
   const taregt = "media/";
   const index = url.indexOf(taregt) + taregt.length;
   return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
