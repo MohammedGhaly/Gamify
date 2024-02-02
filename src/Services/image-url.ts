9f63f6d6ac3a7899c2dfe86a98e5140e5f105f7c
MohammedGhaly <medooghaly7@gmail.com>
2024-02-02 14:41:17 +0200

    handle games with no image

--- src/Services/image-url.ts
+++ src/Services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/image_placeholder.jpg";
+
 function getCroppedUrl(url: string, width: number = 600, height: number = 400) {
-  if (!url) return "";
+  if (!url) return noImage;
   const taregt = "media/";
   const index = url.indexOf(taregt) + taregt.length;
   return url.slice(0, index) + `crop/${width}/${height}/` + url.slice(index);
